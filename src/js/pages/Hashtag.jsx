1cde2e21f814e9fa4379e9c51333c0bcd5901932
danieljauch <daniel.p.jauch@gmail.com>
2018-06-05 14:21:56 +1200

    Added the charts, but still may need tweaking to match the design

--- src/js/pages/Hashtag.jsx
+++ src/js/pages/Hashtag.jsx
@@ -2,6 +2,7 @@
 // Module imports
 import React, { Component, Fragment } from "react"
 import Cookies from "js-cookie"
+import { Line, Pie } from "react-chartjs-2"
 
 // Components
 import Page from "../components/Page"
@@ -15,12 +16,62 @@ import logo from "../../img/logo.png"
 import steeringWheel from "../../img/steering-wheel.png"
 /*** [end of imports] ***/
 
+const GLOBALS = {
+  MAX_HON3Y: 5,
+  orange: "#f39c12",
+  orangeDark: "#e67d23",
+  yellow: "#e9da0d"
+}
+
 export default class Hashtag extends Component {
   state = {
     userId: Cookies.get("userId") || 1,
     hashtag: this.props.match.params.hashtag || "driving",
-    tab: this.props.match.params.tab || "Build Rep",
-    score: 4.12
+    tab: this.props.match.params.tab || "Analytics", // "Build Rep"
+    score: 4.12,
+    lineChartData: null,
+    pieChartData: null
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      lineChartData: {
+        labels: [1, 5, 10, 15, 20, 25, 30], // this is the bit that will change
+        datasets: [
+          {
+            label: "HON3Y",
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: GLOBALS.orangeDark,
+            borderColor: GLOBALS.orange,
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: GLOBALS.orangeDark,
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: GLOBALS.orangeDark,
+            pointHoverBorderColor: "rgba(255, 255, 255, .5)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [0, 3.5, 3, 3.12, 3.75, 3.8, 4.12] // this is the bit that will change
+          }
+        ]
+      },
+      pieChartData: {
+        labels: ["Tesla", "Games", "Apps"],
+        datasets: [
+          {
+            data: [65, 15, 25],
+            backgroundColor: [GLOBALS.orangeDark, GLOBALS.orange, GLOBALS.yellow],
+            hoverBackgroundColor: [GLOBALS.orangeDark, GLOBALS.orange, GLOBALS.yellow]
+          }
+        ]
+      }
+    })
   }
 
   changeTab = tab => {
@@ -30,7 +81,7 @@ export default class Hashtag extends Component {
   }
 
   render() {
-    const { score, hashtag, tab } = this.state
+    const { score, hashtag, tab, lineChartData, pieChartData } = this.state
 
     let buildRepContent = (
       <Fragment>
@@ -41,9 +92,14 @@ export default class Hashtag extends Component {
     )
 
     let analyticsContent = (
-      <Fragment>
-        
-      </Fragment>
+      <div className="chart-wrapper" id="chartjs_hashtag_analytics">
+        <div className="chart-wrap">
+          <Line data={lineChartData} />
+        </div>
+        <div className="chart-wrap">
+          <Pie data={pieChartData} />
+        </div>
+      </div>
     )
 
     let rankingContent = <div>Ranking</div>
@@ -56,9 +112,9 @@ export default class Hashtag extends Component {
             className="hon3y-score"
             style={gradientStyle({
               direction: "top",
-              divisor: 5,
+              divisor: GLOBALS.MAX_HON3Y,
               dividend: score,
-              startColor: "#f39c12",
+              startColor: GLOBALS.orange,
               endColor: "transparent"
             })}>
             {score}
