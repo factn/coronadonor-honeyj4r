643beda9a07275ace7bac7003c823942b5ad2e20
danieljauch <daniel.p.jauch@gmail.com>
2018-06-18 11:23:46 +1200

    Corrected Hon3y mixin. Added Wallet to nav.

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -3,7 +3,7 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import Icon from "@fortawesome/react-fontawesome"
-import { faUser } from "@fortawesome/fontawesome-free-solid"
+import { faUser, faWallet } from "@fortawesome/fontawesome-free-solid"
 
 // Images
 import logo from "../../img/logo.png"
@@ -12,11 +12,14 @@ import logoInactive from "../../img/logo-inactive.png"
 
 const Header = props => (
   <header className={props.className ? `app-header ${props.className}` : "app-header"}>
-    <Link to="/profile" className={props.path === "/profile" ? "profile-btn active" : "profile-btn"}>
+    <Link to="/profile" className={props.path === "/profile" ? "header-btn profile-btn active" : "header-btn profile-btn"}>
       <Icon icon={faUser} />
     </Link>
-    <Link to="/" className={props.path === "/" ? "hon3y-btn active" : "hon3y-btn"}>
+    <Link to="/" className={props.path === "/" ? "header-btn hon3y-btn active" : "header-btn hon3y-btn"}>
       <img src={props.path === "/" ? logo : logoInactive} className="logo" alt="J4R" />
+    </Link>
+    <Link to="/wallet" className={props.path === "/wallet" ? "header-btn wallet-btn active" : "header-btn wallet-btn"}>
+      <Icon icon={faWallet} />
     </Link>
   </header>
 )
