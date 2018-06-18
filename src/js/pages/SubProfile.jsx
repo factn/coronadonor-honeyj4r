33740c9701e30490226cce9a45725ddacfd18a14
danieljauch <daniel.p.jauch@gmail.com>
2018-06-19 11:04:29 +1200

    Missed the subprofile page, that's fixed now.

--- src/js/pages/SubProfile.jsx
+++ src/js/pages/SubProfile.jsx
@@ -10,13 +10,16 @@ import Page from "../components/Page"
 // Utilities
 import Database from "../resources/Database"
 import { unvaluify } from "../resources/Util"
+
+// Images
+import profile1 from "../../img/profile1.png"
 /*** [end of imports] ***/
 
 export default class SubProfile extends Component {
   state = {
     userId: Cookies.get("userId") || 1,
     userData: null,
-    subprofileName: this.props.match.params.subprofile_name,
+    subprofileName: this.props.match.params.subprofile_name
   }
 
   componentDidMount = () => {
@@ -58,20 +61,27 @@ export default class SubProfile extends Component {
     ]
 
     return (
-      <Page className="sub-profile-page" path={this.props.location.pathname} noHeader={true} backHeader={this.props.history.goBack}>
+      <Page
+        className="sub-profile-page"
+        path={this.props.location.pathname}
+        noHeader={true}
+        backHeader={this.props.history.goBack}>
         <section className="profile-section avatar-section">
           <header className="profile-header">
             <h4 className="profile-title">Sub Profile</h4>
           </header>
           <article className="profile-badge">
-            <div className="profile-avatar" />
+            <div
+              className="profile-avatar"
+              style={{
+                backgroundImage: `url("${profile1}")`
+              }}
+            />
             <div className="profile-name">{unvaluify(subprofileName)}</div>
           </article>
         </section>
         <section className="profile-section">
-          <ul className="hashtag-list">
-            {hashtags.map((tag, _index) => <HashTag {...tag} key={_index} />)}
-          </ul>
+          <ul className="hashtag-list">{hashtags.map((tag, _index) => <HashTag {...tag} key={_index} />)}</ul>
         </section>
       </Page>
     )
@@ -80,7 +90,7 @@ export default class SubProfile extends Component {
 
 const HashTag = props => (
   <li className="hashtag-item card">
-    <Link 
+    <Link
       className={`tag ${(props.score >= 4 && "high") ||
         (props.score >= 3 && props.score < 4 && "med") ||
         (props.score < 3 && "low")}-tag`}
